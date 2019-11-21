a5a01c9433ead9d5bd18cc64963b30fc890782b2
Fernando <kevin.gomez@climate.com>
2019-11-21 06:35:21 -0600

    add redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const componseEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, componseEnhancers);
 
 ReactDOM.render(
   <Provider store={store}>
